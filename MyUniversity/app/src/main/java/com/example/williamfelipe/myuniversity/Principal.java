ba467b7e58cf498c36449f4edb3b6883d30a3fa8
FELLSAT <willysaty1393@hotmail.com>
2016-07-14 23:58:13 -0500

    DB
    nueva estructura de db

--- MyUniversity/app/src/main/java/com/example/williamfelipe/myuniversity/Principal.java
+++ MyUniversity/app/src/main/java/com/example/williamfelipe/myuniversity/Principal.java
@@ -5,14 +5,21 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
+
+import java.util.ArrayList;
 
 /**
  * Created by WilliamFelipe on 13/07/2016.
  */
-public class Principal extends AppCompatActivity {
-    String direccionConsultaMaterias = "http://vawdb.freeoda.com/VAW/Consulta_materias.php";
-    ListView listaDeMaterias;
+public class Principal extends AppCompatActivity implements ManejoDB.OnPostExecute{
+
+    String direccionConsultaMaterias = "http://vawdb.freeoda.com/VAW/Consulta_materias.php"; // direccion del los datos en JSON
+    ListView listaDeMaterias; // lista donde se mostraran los datos de la consulta
+    ArrayList arrayList = new ArrayList(); // array donde se depositaran los datos de la consulta
+
+    ManejoDB manejoDB = new ManejoDB("CONSULTA","nomMateria",direccionConsultaMaterias,"");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,28 +27,24 @@ public class Principal extends AppCompatActivity {
         requestWindowFeature(getWindow().FEATURE_NO_TITLE);
         setContentView(R.layout.principal);
 
-        listaDeMaterias = (ListView) findViewById(R.id.lista_de_materias);
-        String tipoQuery = "consulta";
-        String queHacer = "listarMaterias";
+        listaDeMaterias = (ListView) findViewById(R.id.lista_de_materias);//cast de la lista donde mostraremos la consulta
 
-        String postExecute = "nomMateria";
-        final ManejoDB manejoDB = new ManejoDB(tipoQuery,this,postExecute, listaDeMaterias);
-        System.out.println("Execute");
-        manejoDB.execute(queHacer,direccionConsultaMaterias);
+        //trae los nombres de las materias matriculaas y las muestra en el listView
+        manejoDB.delegate = this;
+        manejoDB.execute();
 
-        String postExecute2 = "idMateria";
-        final ManejoDB manejoDB1 = new ManejoDB(tipoQuery,postExecute2);
-        manejoDB1.execute(queHacer,direccionConsultaMaterias);
+        //Traer los id de cada materia
+        final ManejoDB manejoDB1 = new ManejoDB("CONSULTA","idMateria",direccionConsultaMaterias,"");
+        manejoDB1.execute();
 
-        String posExecute3 = "idMateriaMatriculada";
-        final ManejoDB manejoDB2 = new ManejoDB(tipoQuery,posExecute3);
-        manejoDB2.execute(queHacer,direccionConsultaMaterias);
+        //el id que tiene la matriculada la materia
+        final ManejoDB manejoDB2 = new ManejoDB("CONSULTA","idMateriaMatriculada",direccionConsultaMaterias,"");
+        manejoDB2.execute();
 
         listaDeMaterias.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                 Intent intent = new Intent(getApplicationContext(),Calcula_notas.class);
-                System.out.println("intent");
                 intent.putExtra("nombreMateria",manejoDB.getListaFinal().get(position));
                 intent.putExtra("codigoMateria",manejoDB1.getListaFinal().get(position));
                 intent.putExtra("codigoMatricula",manejoDB2.getListaFinal().get(position));
@@ -49,8 +52,15 @@ public class Principal extends AppCompatActivity {
             }
         });
 
+
     }
 
 
-
+    @Override
+    public void Execute() {
+        arrayList = manejoDB.getListaFinal();
+        ArrayAdapter arrayAdapter;
+        arrayAdapter = new ArrayAdapter<String>(this,android.R.layout.simple_expandable_list_item_1,arrayList);
+        listaDeMaterias.setAdapter(arrayAdapter);
+    }
 }
