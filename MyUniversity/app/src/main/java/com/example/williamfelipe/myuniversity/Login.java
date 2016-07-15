0d6ce9f8bea608cba41733eb14b7c75bc31a6ff7
vlaxb <vladix88@gmail.com>
2016-07-15 00:45:23 -0500

    login g+
    acreditacion de google para el acceso

--- MyUniversity/app/src/main/java/com/example/williamfelipe/myuniversity/Login.java
+++ MyUniversity/app/src/main/java/com/example/williamfelipe/myuniversity/Login.java
@@ -1,7 +1,11 @@
 package com.example.williamfelipe.myuniversity;
 
+import android.support.v7.app.AppCompatActivity;
 /**
  * Created by user on 13/07/2016.
  */
-public class Login {
+public class Login extends AppCompatActivity {
+
+
+
 }
